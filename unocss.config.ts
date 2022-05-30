31c6261fad30aa2ca42f141c86897373b6f901fa
THANATHORN <99862291+THANABusiness@users.noreply.github.com>
2022-05-30 09:34:24 +0700

    Initialize refactor/themes

---
+++ unocss.config.ts
@@ -0,0 +1,47 @@
+import {
+  transformerVariantGroup,
+  transformerDirectives,
+  presetAttributify,
+  defineConfig,
+  presetMini,
+  presetUno
+} from "unocss";
+
+// https://github.com/unocss/unocss#readme
+export default defineConfig({
+  presets: [presetMini({ dark: "class" }), presetAttributify(), presetUno()],
+  transformers: [transformerDirectives(), transformerVariantGroup()],
+  exclude: [`${__dirname}/node_modules/**/*`],
+  shortcuts: {
+    "wh-full": "w-full h-full",
+    "cp-on": "cursor-pointer outline-none",
+    "flex-c": "flex justify-center items-center",
+    "flex-ac": "flex justify-around items-center",
+    "flex-bc": "flex justify-between items-center",
+    "navbar-bg-hover":
+      "dark:color-white hover:bg-[#f6f6f6] dark:hover:bg-[#242424]"
+  },
+  theme: {
+    colors: {
+      primary_light_9: "var(--el-color-primary-light-9)",
+      primary: "var(--el-color-primary)",
+      bg_color: "var(--el-bg-color)",
+      text_color_primary: "var(--el-text-color-primary)",
+      text_color_regular: "var(--el-text-color-regular)",
+      text_color_disabled: "var(--el-text-color-disabled)"
+    },
+    backgroundColor: {
+      dark: "#020409"
+    },
+    transitionProperty: [
+      "width",
+      "height",
+      "background",
+      "background-color",
+      "padding-left",
+      "border-color",
+      "right",
+      "fill"
+    ]
+  }
+});
